1e0ad20e6dc89a9474924df68835a84d5028705d
jewelxy <dhs.shahin@gmail.com>
2023-01-02 16:03:09 +0600

    Assignment Variable and Data type

---
+++ javaassignment/Product.java
@@ -0,0 +1,19 @@
+package javaassignment.javaassignment;
+
+public class Product {
+    public static void main(String[] args) {
+        //Assign Data
+        int id = 101;
+        String title = "iphone15";
+        double price = 1985;
+        String description = "Perfect product with best image quality";
+        String category = "phone";
+        
+        //Print Data
+        System.out.println("id = "+id);
+        System.out.println("title = "+title);
+        System.out.println("price = "+price+" euros");
+        System.out.println("description = "+description);
+        System.out.println("category = "+category);
+    }
+}
